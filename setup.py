88a5dbd748293a1dc6488cad45f5ed4885b71245
joaquinseguraellis <57773288+joaquinseguraellis@users.noreply.github.com>
2024-02-23 14:38:57 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,22 @@
 from setuptools import setup
 
-setup()
+setup(
+    name="pydrodesign-joaquin.segura.ellis",
+    version="1.0.0",
+    author="Joaquin Sebastian Segura Ellis",
+    author_email="joaquin.segura.ellis@mi.unc.edu.ar",
+    url="https://github.com/JoaquinSeguElli/pydrodesign",
+    description="Tools for Hydrology Design.",
+    packages=find_packages(),
+    classifiers=[
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Hydrology",
+    ],
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+)
