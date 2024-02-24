04c38bb4afa85ec5f80c2a82bd61a3f7ad27de3c
joaquinseguraellis <57773288+joaquinseguraellis@users.noreply.github.com>
2024-02-24 01:34:59 -0300

    update .

--- src/RiSA/main.py
+++
@@ -1,8 +0,0 @@
-from .tools import *
-from importlib.resources import files
-
-def f():
-    return np.random.random((231, 350, 3, 48))
-
-def get():
-    return bin_file(files('RiSA.data').joinpath('prueba'), f)
