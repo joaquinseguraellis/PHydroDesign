eb54443896240f612bb5d592c72a9c38fa28537c
joaquinseguraellis <57773288+joaquinseguraellis@users.noreply.github.com>
2024-02-24 00:08:34 -0300

    Update main.py

--- src/RiSA/main.py
+++ src/RiSA/main.py
@@ -3,5 +3,5 @@ from .tools import *
 def f():
     return np.random.random((231, 350, 3, 48))
 
-if __name__=='__main__':
-    data = bin_file(Path('prueba'), f)
+def get():
+    return bin_file(Path('prueba'), f)
