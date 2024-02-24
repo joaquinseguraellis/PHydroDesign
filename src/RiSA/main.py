d06b74a2f31cea8b17b2d0e165f8f14e9c721e6d
joaquinseguraellis <57773288+joaquinseguraellis@users.noreply.github.com>
2024-02-24 01:09:17 -0300

    update .

--- src/RiSA/main.py
+++ src/RiSA/main.py
@@ -5,4 +5,4 @@ def f():
     return np.random.random((231, 350, 3, 48))
 
 def get():
-    return bin_file(files('RiSA.output').joinpath('prueba'), f)
+    return bin_file(files('RiSA.data').joinpath('prueba'), f)
