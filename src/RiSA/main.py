c7eb31aa6735c6d79d9e374e3948a0143d34ad0a
joaquinseguraellis <57773288+joaquinseguraellis@users.noreply.github.com>
2024-02-24 00:20:07 -0300

    add .

--- src/RiSA/main.py
+++ src/RiSA/main.py
@@ -1,7 +1,8 @@
 from .tools import *
+from importlib.resources import files
 
 def f():
     return np.random.random((231, 350, 3, 48))
 
 def get():
-    return bin_file(Path('prueba'), f)
+    return bin_file(files('mypkg.data').joinpath('prueba'), f)
